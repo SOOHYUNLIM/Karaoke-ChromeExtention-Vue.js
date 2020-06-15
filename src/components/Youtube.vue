d6c22a674036e79f61054e422c6fd8279b41f175
SOOHYUNLIM <sh_7682@naver.com>
2020-06-15 22:26:55 +0900

    youtube iframe 추가

--- src/components/Youtube.vue
+++ src/components/Youtube.vue
@@ -1,24 +1,27 @@
 <template>
-  <div>
-      <h1>YoutubePage</h1>
-      <p>{{youtube}}</p>
-  </div>
+    <div>
+        <h1>YoutubePage</h1>
+        <p>{{youtube}}</p>
+        <iframe id="player" type="text/html" width="640" height="360"
+                src="http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com"
+                frameborder="0"></iframe>
+    </div>
 </template>
 
 <script>
-import EventBus from "../util/EventBus"
+    import EventBus from "../util/EventBus"
 
-export default {
-  name: "Youtube",
-  data() {
-    return {
-      youtube: ""
+    export default {
+        name: "Youtube",
+        data() {
+            return {
+                youtube: ""
+            }
+        },
+        created() {
+            EventBus.$on("song", ({videoId}) => this.videoId = videoId)
+        }
     }
-  },
-  created() {
-    EventBus.$on("song", ({videoId}) => this.videoId = videoId)
-  }
-}
 </script>
 
 <style>
